d0be31d6385882146d44ceda29fea5d5b821c4c0
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-16 22:29:39 -0500

    Loops

--- src/javaFundamentals/controlStructures/loops/exercises/Square.java
+++ src/javaFundamentals/controlStructures/loops/exercises/Square.java
@@ -1,2 +1,15 @@
-package javaFundamentals.controlStructures.loops.exercises;public class Square {
+package javaFundamentals.controlStructures.loops.exercises;
+
+import javax.swing.*;
+
+public class Square {
+    public static void main(String[] args) {
+        var number = Integer.parseInt(JOptionPane.showInputDialog("Enter a number here: "));
+
+        while (number >= 0){
+            var square = (int) Math.pow(number, 2);
+            JOptionPane.showMessageDialog(null, "Square of "+number+" is "+square);
+            number = Integer.parseInt(JOptionPane.showInputDialog("Enter another number here: "));
+        }
+    }
 }
