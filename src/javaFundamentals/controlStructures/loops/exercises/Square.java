aa87ff279e008a8238f498ce96c65bfd70c8771b
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-16 22:34:15 -0500

    Loops

--- src/javaFundamentals/controlStructures/loops/exercises/Square.java
+++ src/javaFundamentals/controlStructures/loops/exercises/Square.java
@@ -1,6 +1,6 @@
 package javaFundamentals.controlStructures.loops.exercises;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class Square {
     public static void main(String[] args) {
