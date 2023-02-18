b72ec676e31f19e20cb8092d94be1c5109118d4d
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-18 08:16:47 -0500

    Loops

---
+++ src/javaFundamentals/controlStructures/loops/exercises/ShowFrom1toN.java
@@ -0,0 +1,17 @@
+package javaFundamentals.controlStructures.loops.exercises;
+
+import javax.swing.JOptionPane;
+
+public class ShowFrom1toN {
+    public static void main(String[] args) {
+        /*
+        * Ask for a 'n' number, and display from 1 to n
+        * */
+
+        int number = Integer.parseInt(JOptionPane.showInputDialog("Enter a number here: "));
+
+        for (int i = 1; i <= number; i++){
+            JOptionPane.showMessageDialog(null, "Number: "+i);
+        }
+    }
+}
