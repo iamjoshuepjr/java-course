4c7414ccfb6bff47a019ef0346938188d83091fd
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-17 22:07:16 -0500

    Loops

---
+++ src/javaFundamentals/controlStructures/loops/exercises/NumberCounter.java
@@ -0,0 +1,17 @@
+package javaFundamentals.controlStructures.loops.exercises;
+
+import javax.swing.JOptionPane;
+
+public class NumberCounter {
+    public static void main(String[] args) {
+        /* Ask for a number until it is a negative number, and display how many number are entered */
+        var number = Integer.parseInt(JOptionPane.showInputDialog("Enter a number here: "));
+        var count = 0;
+
+        while (number >= 0){
+            number = Integer.parseInt(JOptionPane.showInputDialog("Enter another number here: "));
+            count += 1;
+        }
+        JOptionPane.showMessageDialog(null, "You have entered ("+count+") numbers! ");
+    }
+}
