f565a37c8811816929c4e1b16eeb9c7f2a181997
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-17 23:38:20 -0500

    Loops

--- src/javaFundamentals/controlStructures/loops/exercises/GuessRandomNumber.java
+++ src/javaFundamentals/controlStructures/loops/exercises/GuessRandomNumber.java
@@ -1,4 +1,28 @@
 package javaFundamentals.controlStructures.loops.exercises;
 
-public class GuessRadomNumber {
+import javax.swing.JOptionPane;
+
+public class GuessRandomNumber {
+    public static void main(String[] args) {
+        /*
+        * Make a game to guess a number.
+        * To do this, generate a random number between 0 -  100, then indicate "is greater" or "is less"
+        * with respect to N. The process ends when the user hits. Show the number of attempts.
+        * */
+        var random = (int) Math.random() * 100;
+        var counter = 0;
+        byte number;
+
+        do {
+            number = Byte.parseByte(JOptionPane.showInputDialog("Enter a number here: "));
+            if(random > number){
+                JOptionPane.showMessageDialog(null, "Enter a greater number.");
+            }else {
+                JOptionPane.showMessageDialog(null, "Enter a less number.");
+            }
+            counter++;
+        }
+        while (number != random);
+        JOptionPane.showMessageDialog(null, "Attempts: "+counter);
+    }
 }
