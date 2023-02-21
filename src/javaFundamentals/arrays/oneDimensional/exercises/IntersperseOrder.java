6e8df0db347b7315a057ca13a4f43808b8add675
joshuepjr <mycoursesonline2021@gmail.com>
2023-02-20 19:53:05 -0500

    Arrays

---
+++ src/javaFundamentals/arrays/oneDimensional/exercises/IntersperseOrder.java
@@ -0,0 +1,19 @@
+package javaFundamentals.arrays.oneDimensional.exercises;
+
+import javax.swing.*;
+
+public class IntersperseOrder {
+    public static void main(String[] args) {
+
+        byte[] order = new byte[10];
+
+        for (byte i = 0; i < order.length; i++) {
+            order[i] = Byte.parseByte(JOptionPane.showInputDialog("Enter the ("+(i+1)+") number here: "));
+        }
+
+        for (byte i = 0; i < (order.length/2); i++) {
+            System.out.println(order[i]);
+            System.out.println(order[order.length-1] - i);
+        }
+    }
+}
