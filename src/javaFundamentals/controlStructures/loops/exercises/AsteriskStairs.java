d0224ef029789a6c9f2161f2352c44b68e80f60d
joshuepjr <mycoursesonline2021@gmail.com>
2023-03-08 12:58:55 -0500

    Loops

--- src/javaFundamentals/controlStructures/loops/exercises/AsteriskStairs.java
+++ src/javaFundamentals/controlStructures/loops/exercises/AsteriskStairs.java
@@ -1,2 +1,16 @@
-package javaFundamentals.controlStructures.loops.exercises;public class AsteriskStairs {
+package javaFundamentals.controlStructures.loops.exercises;
+
+import javax.swing.*;
+
+public class AsteriskStairs {
+    public static void main(String[] args) {
+        int height = Integer.parseInt(JOptionPane.showInputDialog("Stairs Height: "));
+
+        for (int asterisk = 1; asterisk <= height; asterisk++){
+            for (int j = 0; j < asterisk; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
