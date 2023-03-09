a6f528f676e4171540c8b664222f1be94b1ffa8e
joshuepjr <mycoursesonline2021@gmail.com>
2023-03-09 12:52:51 -0500

    Arrays

--- src/javaFundamentals/arrays/oneDimensional/exercises/BackwardString.java
+++ src/javaFundamentals/arrays/oneDimensional/exercises/BackwardString.java
@@ -1,2 +1,19 @@
-package javaFundamentals.arrays.oneDimensional.exercises;public class BackwardString {
+package javaFundamentals.arrays.oneDimensional.exercises;
+
+import javax.swing.JOptionPane;
+
+public class BackwardString {
+    public static void main(String[] args) {
+        var text = JOptionPane.showInputDialog("Enter a text here: ");
+        var length = text.length();
+
+        char[] character = new  char[length];
+        var inverse = length;
+
+        for (byte i = 0; i < length; i++) {
+            character[i] = text.charAt(inverse -1);
+            inverse--;
+        }
+        System.out.println(character);
+    }
 }
