1fdf0c49b4734df6bd97ee62659e19ac57fba058
joshuepjr <mycoursesonline2021@gmail.com>
2023-03-09 17:54:09 -0500

    Arrays

--- src/javaFundamentals/arrays/oneDimensional/exercises/BackwardString.java
+++ src/javaFundamentals/arrays/oneDimensional/exercises/BackwardString.java
@@ -5,8 +5,10 @@ import javax.swing.JOptionPane;
 public class BackwardString {
     public static void main(String[] args) {
         var text = JOptionPane.showInputDialog("Enter a text here: ");
+        // Calculating length
         var length = text.length();
 
+        // Creating Array
         char[] character = new  char[length];
         var inverse = length;
 
