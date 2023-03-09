d0da400a0343768afc5ffd395d79080fd355268a
joshuepjr <mycoursesonline2021@gmail.com>
2023-03-09 12:06:17 -0500

    Loops

--- src/javaFundamentals/controlStructures/loops/exercises/Fibonacci.java
+++ src/javaFundamentals/controlStructures/loops/exercises/Fibonacci.java
@@ -1,2 +1,19 @@
-package javaFundamentals.controlStructures.loops.exercises;public class Fibonacci {
+package javaFundamentals.controlStructures.loops.exercises;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        byte a = 0, b = 1, c = 0;
+        for (int i = 0; i < 10; i++) {
+            if(i < 9){
+                System.out.print(a + ",");
+                c = (byte) (a + b);
+                a = b;
+                b = c;
+            }
+            else {
+                System.out.println(a);
+            }
+        }
+
+    }
 }
