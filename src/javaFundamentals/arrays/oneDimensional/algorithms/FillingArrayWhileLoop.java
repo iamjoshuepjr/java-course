ba9ea11fbfe5ea750e92c895fe191762a685214e
joshuepjr <mycoursesonline2021@gmail.com>
2023-03-16 21:55:58 -0500

    Filling Array While Loop

--- src/javaFundamentals/arrays/oneDimensional/algorithms/FillingArrayWhileLoop.java
+++ src/javaFundamentals/arrays/oneDimensional/algorithms/FillingArrayWhileLoop.java
@@ -1,2 +1,25 @@
-package javaFundamentals.arrays.oneDimensional.algorithms;public class FillingArrayForWhileLoop {
+package javaFundamentals.arrays.oneDimensional.algorithms;
+
+import javax.swing.*;
+
+public class FillingArrayWhileLoop {
+    public static void main(String[] args) {
+        // Welcome message
+        JOptionPane.showMessageDialog(null, "Welcome to Programming Academy!");
+
+        // creating arrays
+        byte index = 15;
+        short[] codes = new short[index];
+        String[] names = new String[index];
+
+        // auxiliaries variables
+        var more = "";
+        var elements = 0;
+        while (){
+
+        }
+        // asking for the numbers of elements to enter
+
+
+    }
 }
