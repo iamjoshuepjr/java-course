14acb1f210e3f200fad4e7c48d234ee54d886890
joshuepjr <mycoursesonline2021@gmail.com>
2023-03-21 21:32:01 -0500

    Arrays Class Methods

---
+++ src/javaFundamentals/arrays/oneDimensional/sorting/methods/SearchMethod.java
@@ -0,0 +1,18 @@
+package javaFundamentals.arrays.oneDimensional.sorting.methods;
+import java.util.Arrays;
+
+public class SearchMethod {
+    public static void main(String[] args) {
+        byte ages[] = {12, 2, 24, 51, 19, 15};
+        System.out.println("The original ages array: " + Arrays.toString(ages));
+
+        // sorting
+        Arrays.sort(ages);
+        System.out.println("The sorted ages array: " + Arrays.toString(ages));
+
+        // searching
+        byte key = 15;
+        int index = Arrays.binarySearch(ages, key);
+        System.out.println("The age " +key+ " was found at ["+index+"] position!");
+    }
+}
---
+++ src/javaFundamentals/arrays/oneDimensional/sorting/methods/SortMethod.java
@@ -0,0 +1,34 @@
+package javaFundamentals.arrays.oneDimensional.sorting.methods;
+
+/***
+ * Array class is a class containing static methods that are used with arrays to
+ * + search
+ * + sort
+ * + compare
+ * + insert elements
+ * + return a string representation
+ *
+ */
+
+import java.util.Arrays;
+
+public class SortMethod {
+    public static void main(String[] args) {
+        char vowels[] = {'e', 'u', 'o', 'i', 'a' };
+        System.out.println("The original vowels array: " + Arrays.toString(vowels));
+
+        // using the .sort() method to sort it
+        Arrays.sort(vowels);
+
+        // The sorted array
+        System.out.println("The sorted vowels array: " + Arrays.toString(vowels));
+
+        byte ages[] = {23, 44, 18, 24, 15, 56, 99, 23};
+        System.out.println("The original ages array: " + Arrays.toString(ages));
+        System.out.println("Length: " + ages.length);
+
+        // using .sort() method to sort in a specific range
+        Arrays.sort(ages, 3, 7);
+        System.out.println("The sorted ages array: " + Arrays.toString(ages));
+    }
+}
