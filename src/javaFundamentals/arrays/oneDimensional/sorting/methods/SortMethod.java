6c4140647eb0688f0cda2cf386ee7b043e28a441
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-01 17:28:15 -0500

    Sort Method

--- src/javaFundamentals/arrays/oneDimensional/sorting/methods/SortMethod.java
+++ src/javaFundamentals/arrays/oneDimensional/sorting/methods/SortMethod.java
@@ -29,6 +29,6 @@ public class SortMethod {
 
         // using .sort() method to sort in a specific range
         Arrays.sort(ages, 2, 7);
-        System.out.println("The sorted ages array between indexes 2 - 7 : " + Arrays.toString(ages));
+        System.out.println("The sorted ages array between indexes 2 - 6 : " + Arrays.toString(ages));
     }
 }
