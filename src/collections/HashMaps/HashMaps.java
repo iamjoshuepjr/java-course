4114c005c9a1760f60599f3089dc3cc5986dc2b3
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-02 10:23:05 -0500

    Java Collections

---
+++ src/collections/HashMaps/HashMaps.java
@@ -0,0 +1,13 @@
+package javaFundamentals.HashMaps;
+
+public class HashMaps {
+    public static void main(String[] args) {
+        /**
+         * The HasMap class of the Java Collection framework provides
+         * the functionality of the hash table data structure.
+         * It stores elements in key/value pairs.
+         * Here, key are unique identifiers used to associate each value on a map.
+         * The HasMap class implements the Map interface.
+         */
+    }
+}
