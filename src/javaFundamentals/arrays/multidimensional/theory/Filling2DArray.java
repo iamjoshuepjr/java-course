3144f577482bd2b69da5475e64b285e8b1f1f6c4
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-04 11:17:52 -0500

    Multidimensional Array

--- src/javaFundamentals/arrays/multidimensional/theory/Filling2DArray.java
+++ src/javaFundamentals/arrays/multidimensional/theory/Filling2DArray.java
@@ -6,25 +6,33 @@ public class Filling2DArray {
     public static void main(String[] args) {
 
         // asking for rows/arrays elements/indexes
+        JOptionPane.showMessageDialog(null, "The following numbers you are going to enter must be:\nNo greater than 127\nNo less than -128");
         var arrays = Byte.parseByte(JOptionPane.showInputDialog("Enter the number of elements you want as rows/arrays: "));
         var indexes = Byte.parseByte(JOptionPane.showInputDialog("Enter the number of elements you want as columns/indexes: "));
 
         // creating the 2D array
-        String[][] cars = new String[arrays][indexes];
+        String[][] elements = new String[arrays][indexes];
 
         // filling array with for loop
-        for (int i = 0; i < cars.length; i++) {
-            for (int j = 0; j < cars[i].length; j++) {
-                cars[i][j] = JOptionPane.showInputDialog("Enter the ["+i+"]["+j+"] car: ");
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = 0; j < elements[i].length; j++) {
+                elements[i][j] = JOptionPane.showInputDialog("Enter the ["+i+"]["+j+"] element: ");
             }
         }
 
         // printing array with for loop
-        for (int i = 0; i < cars.length; i++) {
-            for (int j = 0; j < cars[i].length; j++) {
-                System.out.println("Array ["+i+"] - Index ["+j+"] " +cars[i][j]);
+        byte iter = 0;
+        for (int i = 0; i < elements.length; i++) {
+            System.out.print("Array [" + iter + "] ");
+            for (int j = 0; j < elements[i].length; j++) {
+                if(j == 0){
+                    System.out.print(" index[" + j + "] " + elements[i][j]);
+                } else {
+                    System.out.print(" - index[" + j + "] " + elements[i][j]);
+                }
             }
             System.out.println("");
+            iter++;
         }
     }
 }
