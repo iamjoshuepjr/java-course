8958fe5f22a3be62c8dc1a5be05d9746cebb23f8
joshuepjr <mycoursesonline2021@gmail.com>
2023-04-04 19:08:10 -0500

    Multidimensional Arrays

--- src/javaFundamentals/arrays/multidimensional/exercises/TransposedMatrix.java
+++ src/javaFundamentals/arrays/multidimensional/exercises/TransposedMatrix.java
@@ -1,2 +1,46 @@
-package javaFundamentals.arrays.multidimensional.exercises;public class TrasnposedMatrix {
+package javaFundamentals.arrays.multidimensional.exercises;
+
+import javax.swing.*;
+
+public class TransposedMatrix {
+    public static void main(String[] args) {
+        /**
+         * Creates and fill a 2D array and display its transposed matrix
+         */
+
+        byte[][] matrix = new byte[3][3];
+
+        for (byte i = 0; i < matrix.length; i++) {
+            for (byte j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = Byte.parseByte(JOptionPane.showInputDialog("Enter the ["+i+"]["+j+"] element: "));
+            }
+        }
+
+        System.out.println("Original Matrix");
+        for (byte i = 0; i < matrix.length; i++) {
+            for (byte j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+        // transposing the array
+        byte aux;
+        for (byte i = 0; i < matrix.length; i++) {
+            for (byte j = 0; j < i; j++) {
+                aux = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = aux;
+            }
+            System.out.println();
+        }
+
+        System.out.println("Transposed Matrix");
+        for (byte i = 0; i < matrix.length; i++) {
+            for (byte j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/javaFundamentals/arrays/multidimensional/exercises/TransposedMatrix2.java
+++ src/javaFundamentals/arrays/multidimensional/exercises/TransposedMatrix2.java
@@ -1,2 +1,31 @@
-package javaFundamentals.arrays.multidimensional.exercises;public class TransposedMatrix2 {
+package javaFundamentals.arrays.multidimensional.exercises;
+
+public class TransposedMatrix2 {
+    public static void main(String[] args) {
+        /**
+         * Creates and fill a 2D array and display its transposed matrix
+         */
+
+        byte[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Transposed Matrix");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
