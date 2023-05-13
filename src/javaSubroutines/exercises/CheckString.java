aec7d892ee46521dc15285d80259e5ed481a0b9e
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-13 17:05:59 -0500

    Exercises

--- src/javaSubroutines/exercises/CheckString.java
+++ src/javaSubroutines/exercises/CheckString.java
@@ -5,20 +5,20 @@ public class CheckString {
     public static void main(String[] args) {
         var text = "";
         do {
-            text = JOptionPane.showInputDialog("Enter a text here: ");
+            text = JOptionPane.showInputDialog("Enter an integer here: ");
         } while (!isInteger(text));
 
         var integer = Integer.parseInt(text);
         System.out.println("Integer Value: " + integer);
     }
 
-    public static boolean isInteger(String array) {
-        if(array.length() == 0){
+    public static boolean isInteger(String text) {
+        if(text.length() == 0){
             return false;
         }
 
-        for (int i = 0; i < array.length(); i++) {
-            if((array.charAt(i) < '0') || (array.charAt(i) > '9')){
+        for (int i = 0; i < text.length(); i++) {
+            if((text.charAt(i) < '0') || (text.charAt(i) > '9')){
                 return false;
             }
         }
