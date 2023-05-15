fcc19ac762211e1fc817fac9d8e3e649b5748e19
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-15 18:21:13 -0500

    OOP

---
+++ src/javaObjectOrientedProgramming/theory/Car.java
@@ -0,0 +1,36 @@
+package javaObjectOrientedProgramming.theory;
+
+import javax.swing.JOptionPane;
+
+/**
+ * OOP is a programming paradigm that focuses on organizing code into objects that interact with each other.
+ * It provides a structured way to design and build software systems by modeling real-world objects and their relationships.
+ *
+ * Classes and objects:
+ * A class is a blueprint or template that defines the structure and behavior of objects.
+ * An object is an instance of a class that represents a specific entity or concept.
+ * Classes encapsulate data (attributes/fields) and behaviors (methods) into objects.
+ */
+
+// Object Class
+public class Car {
+
+    // attributes
+    String brand;
+    String color;
+    String model;
+    short year;
+
+    // methods
+    public void start() {
+        JOptionPane.showMessageDialog(null, "Staring Car!");
+    }
+
+    public void accelerate() {
+        JOptionPane.showMessageDialog(null, "Accelerating - Velocity: 80 Km/h");
+    }
+
+    public void stop(){
+        JOptionPane.showMessageDialog(null, "Stopping - Velocity: 40 Km/h");
+    }
+}
---
+++ src/javaObjectOrientedProgramming/theory/CarTest.java
@@ -0,0 +1,21 @@
+package javaObjectOrientedProgramming.theory;
+
+// Class Controller
+public class CarTest {
+    public static void main(String[] args) {
+
+        // Instantiating a Car object
+        Car car = new Car();
+
+        // Accessing Car class attributes
+        car.brand = "Ford";
+        car.color = "Orange";
+        car.model = "GT";
+        car.year = 2023;
+
+        // Accessing Car class methods
+        car.start();
+        car.accelerate();
+        car.stop();
+    }
+}
