8fcd659abd907b4620407bac0eb11f934fc8c2a5
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-17 10:32:51 -0500

    OPP - Method Overload

--- src/javaObjectOrientedProgramming/theory/methodOverload/BovineTest.java
+++ src/javaObjectOrientedProgramming/theory/methodOverload/BovineTest.java
@@ -1,13 +1,24 @@
 package javaObjectOrientedProgramming.theory.methodOverload;
 
+// Class Controller
 public class BovineTest {
     public static void main(String[] args) {
-        Bovine bovine = new Bovine("Holstein", 3, 1200.5f);
 
+        // Instantiating Bovine object
+        Bovine bovine = new Bovine("Holstein", 3, 1200.5f);
+        Bovine bovine2 = new Bovine("Jersey", 2, 800.3f);
+
+        // Accessing Bovine Object methods
         bovine.displayInfo();
         bovine.eat();
         bovine.displayInfo();
         bovine.eat("grass");
         bovine.displayInfo();
+
+        bovine2.displayInfo();
+        bovine2.eat();
+        bovine2.displayInfo();
+        bovine2.eat("grass");
+        bovine2.displayInfo();
     }
 }
