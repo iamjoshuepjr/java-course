7c36e9179339b9478fefdc72b0ecf3ffa59c6d61
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-18 22:33:59 -0500

    OPP - Exercises

---
+++ src/javaObjectOrientedProgramming/exercises/calculate/Quadrilateral.java
@@ -0,0 +1,25 @@
+package javaObjectOrientedProgramming.exercises.calculate;
+
+public class Quadrilateral {
+    // Attributes
+    private float side1;
+    private float side2;
+
+    // Constructor
+    public Quadrilateral(float side1, float side2){
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    public Quadrilateral(float side1){
+        this.side1 = this.side2 = side1;
+    }
+
+    public float perimeter(){
+        return 2 * (side1 + side2);
+    }
+
+    public float area(){
+        return side1 * side2;
+    }
+}
---
+++ src/javaObjectOrientedProgramming/exercises/calculate/QuadrilateralTest.java
@@ -0,0 +1,24 @@
+package javaObjectOrientedProgramming.exercises.calculate;
+
+import javax.swing.*;
+
+public class QuadrilateralTest {
+    public static void main(String[] args) {
+        Quadrilateral quadrilateral;
+        float side1, side2;
+
+        side1 = Float.parseFloat(JOptionPane.showInputDialog("Enter the first side: "));
+        side2 = Float.parseFloat(JOptionPane.showInputDialog("Enter the second side: "));
+
+        if(side1 == side2){
+            quadrilateral = new Quadrilateral(side1);
+        }
+        else {
+            quadrilateral = new Quadrilateral(side1, side2);
+        }
+
+        JOptionPane.showMessageDialog(null, "Quadrilateral" +
+                "\nArea: " + quadrilateral.area() +
+                "\nPerimeter: " +quadrilateral.perimeter());
+    }
+}
