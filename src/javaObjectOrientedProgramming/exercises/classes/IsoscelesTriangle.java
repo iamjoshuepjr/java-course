1ec440a3f6b424c6c246358047f9e1f027b0b2c5
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-20 17:00:10 -0500

    OOP - Exercises

--- src/javaObjectOrientedProgramming/exercises/classes/IsoscelesTriangle.java
+++ src/javaObjectOrientedProgramming/exercises/classes/IsoscelesTriangle.java
@@ -1,2 +1,50 @@
-package javaObjectOrientedProgramming.exercises.classes;public class IsoscelesTriangle {
+package javaObjectOrientedProgramming.exercises.classes;
+
+/**
+ * Design a program to work with isosceles triangles.
+ * To do this, define the necessary attributes required,
+ * provide accessor methods, a constructor method,
+ * and implement methods to calculate the perimeter and area of a triangle.
+ * Additionally, implement a method that, given an array of triangles,
+ * returns the area of the triangle with the largest surface.
+ */
+
+// Object Class
+public class IsoscelesTriangle {
+
+    // Attributes
+    private float base;
+    private float side;
+
+    // Constructor
+    public IsoscelesTriangle(float base, float side){
+        this.base = base;
+        this.side = side;
+    }
+
+    // Setter and Getters
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getSide() {
+        return side;
+    }
+
+    public void setSide(float side) {
+        this.side = side;
+    }
+
+    // Perimeter
+    public float perimeter(){
+        return 2 * base + side;
+    }
+
+    public float area(){
+        return (float) (base * Math.sqrt((side * side) - ((base * base)/4)))/2;
+    }
 }
---
+++ src/javaObjectOrientedProgramming/exercises/test/IsoscelesTriangleTest.java
@@ -0,0 +1,47 @@
+package javaObjectOrientedProgramming.exercises.test;
+import javaObjectOrientedProgramming.exercises.classes.IsoscelesTriangle;
+
+import javax.swing.JOptionPane;
+
+// Class Controller
+public class IsoscelesTriangleTest {
+    public static float areaLargestSurface(IsoscelesTriangle[] triangles){
+        float largestArea;
+
+        largestArea = triangles[0].area();
+        for (int i = 1; i <triangles.length; i++) {
+            if(triangles[i].area() > largestArea){
+                largestArea = triangles[i].area();
+            }
+        }
+
+        return largestArea;
+    }
+
+    public static void main(String[] args) {
+        float base, side;
+        byte triangle;
+
+        triangle = Byte.parseByte(JOptionPane.showInputDialog("Please, enter the number of triangles you want to handle."));
+
+        // Creating a triangle array
+        IsoscelesTriangle triangles[] = new IsoscelesTriangle[triangle];
+
+        for (int i = 0; i < triangles.length; i++) {
+            JOptionPane.showMessageDialog(null, "Enter the attributes of the triangle " +(i+1));
+            base = Float.parseFloat(JOptionPane.showInputDialog("Enter the triangle's base"));
+            side = Float.parseFloat(JOptionPane.showInputDialog("Enter the triangle's side"));
+
+            // Instantiating each triangle
+            triangles[i] = new IsoscelesTriangle(base, side);
+
+            // Displaying area and perimeter
+            JOptionPane.showMessageDialog(null, "Area and Perimeter" +
+                    "\nArea: " +triangles[i].area() +
+                    "\nPerimeter: " +triangles[i].perimeter());
+        }
+
+        JOptionPane.showMessageDialog(null, "Area Largest Surface" +
+                "\nArea: " +areaLargestSurface(triangles));
+    }
+}
