f7bc7a6f24b9aa2fe7345cec3b7b2cdf62902a34
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-21 15:07:00 -0500

    OOP - Exercises

---
+++ src/javaObjectOrientedProgramming/exercises/classes/Worker.java
@@ -0,0 +1,37 @@
+package javaObjectOrientedProgramming.exercises.classes;
+
+/**
+ * Create a class that allows calculating the salary of a worker based on their worked hours, knowing that all workers have the same hourly rate.
+ * Create two workers and calculate their salaries. Then modify the hourly rate and recalculate the salaries of the two workers.
+ */
+
+// Object Class
+public class Worker {
+
+    // Instance attributes
+    private String name;
+    private float hourRate;
+
+    // Constructor
+    public Worker(String name, float hourRater){
+        this.name = name;
+        this.hourRate = hourRater;
+    }
+
+    // Getter and Setters
+    public String getName() {
+        return name;
+    }
+
+    public float getHourRate(){
+        return hourRate;
+    }
+
+    public void setHourRate(float hourRate){
+        this.hourRate = hourRate;
+    }
+
+    public float salary(float hoursWorked){
+        return hoursWorked * hourRate;
+    }
+}
---
+++ src/javaObjectOrientedProgramming/exercises/test/WorkerTest.java
@@ -0,0 +1,36 @@
+package javaObjectOrientedProgramming.exercises.test;
+
+import javaObjectOrientedProgramming.exercises.classes.Worker;
+
+import javax.swing.*;
+
+// Controller class
+public class WorkerTest {
+    public static void main(String[] args) {
+        // Instantiating 2 Worker objects
+        Worker worker1 = new Worker("Joshuép Jr.", 35.7f);
+        float hoursWorked1 = Float.parseFloat(JOptionPane.showInputDialog("Enter the hours worked for the first worker"));
+
+        JOptionPane.showMessageDialog(null, "Worker Salary" +
+                "\nName: " + worker1.getName() +
+                "\nSalary $" +worker1.salary(hoursWorked1));
+
+        Worker worker2 = new Worker("Jane.", 35.7f);
+        float hoursWorked2 = Float.parseFloat(JOptionPane.showInputDialog("Enter the hours worked for the second worker"));
+
+        JOptionPane.showMessageDialog(null, "Worker Salary" +
+                "\nName: " + worker2.getName() +
+                "\nSalary $" +worker2.salary(hoursWorked2));
+
+        // Modifying the hourly rate
+        worker1.setHourRate(45.0f);
+        worker2.setHourRate(45.0f);
+
+        // Recalculating the salaries
+        JOptionPane.showMessageDialog(null, "Worker Salary" +
+                "\nName: " + worker1.getName() +
+                "\nSalary $" +worker1.salary(hoursWorked1) +
+                "\nName: " + worker2.getName() +
+                "\nSalary $" +worker2.salary(hoursWorked2));
+    }
+}
