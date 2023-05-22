8004373b25abf4faa1fa9fe49a1df1d71c129e7e
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-21 22:54:31 -0500

    OPP - Access Modifiers

--- src/javaObjectOrientedProgramming/theory/accessModifiers/farm/Farm.java
+++ src/javaObjectOrientedProgramming/theory/accessModifiers/farm/Farm.java
@@ -7,7 +7,7 @@ public class Farm {
         Bovine bovine = new Bovine();
         Chicken chicken = new Chicken();
 
-        // Accessing Bovine class from the Farm Class
+        Accessing Bovine class from the Farm Class
         bovine.breed = "Holstein"; // ERROR: breed is private and is not accessible outside the class.
                                    // We cannot access directly private members from Farm class.
 
@@ -21,9 +21,9 @@ public class Farm {
 
         bovine.eat();              // Accessible from anywhere (public access modifier)
         bovine.displayInfo();
-        bovine.eat("grass");  // ERROR: eat(String food) is private. We cannot access directly private members.
-        bovine.makeSound();        // ERROR: makeSound() is protected. It's only accessible inside the same package or by
-                                   // a subclass in other package.
+        bovine.eat("grass");   // ERROR: eat(String food) is private. We cannot access directly private members.
+        bovine.makeSound();         // ERROR: makeSound() is protected. It's only accessible inside the same package or by
+                                    // a subclass in other package.
 
         bovine.makeSound(5); // ERROR: makeSound(int n_times) has default access. It's not accessible outside its own package
     }
