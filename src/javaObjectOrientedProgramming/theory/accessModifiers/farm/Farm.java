942ce72124efdf4ae03d64ec51229b717accf015
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-21 23:12:00 -0500

    OOP - Access Modifiers

--- src/javaObjectOrientedProgramming/theory/accessModifiers/farm/Farm.java
+++ src/javaObjectOrientedProgramming/theory/accessModifiers/farm/Farm.java
@@ -8,23 +8,23 @@ public class Farm {
         Chicken chicken = new Chicken();
 
         // Accessing Bovine class from the Farm Class
-        //bovine.breed = "Holstein"; // ERROR: breed is private and is not accessible outside the class.
+        bovine.breed = "Holstein"; // ERROR: breed is private and is not accessible outside the class.
                                    // We cannot access directly private members from Farm class.
 
-        //bovine.gender = "Female";  // ERROR: gender is protected and is not accessible from Farm class
+        bovine.gender = "Female";  // ERROR: gender is protected and is not accessible from Farm class
                                    // because Farm class is not a subclass of Bovine or Chicken.
 
-        //bovine.color = "Black";    // ERROR: color has default access and is not accessible from Farm.
+        bovine.color = "Black";    // ERROR: color has default access and is not accessible from Farm.
                                    // It's not accessible outside the package or by subclasses in other packages.
 
         bovine.age = 2;            // Accessible from anywhere (public access modifier)
 
         bovine.eat();              // Accessible from anywhere (public access modifier)
         bovine.displayInfo();
-        //bovine.eat("grass");   // ERROR: eat(String food) is private. We cannot access directly private members.
-        //bovine.makeSound();         // ERROR: makeSound() is protected. It's only accessible inside the same package or by
+        bovine.eat("grass");   // ERROR: eat(String food) is private. We cannot access directly private members.
+        bovine.makeSound();         // ERROR: makeSound() is protected. It's only accessible inside the same package or by
                                     // a subclass in other package.
 
-        //bovine.makeSound(5); // ERROR: makeSound(int n_times) has default access. It's not accessible outside its own package
+        bovine.makeSound(5); // ERROR: makeSound(int n_times) has default access. It's not accessible outside its own package
     }
 }
