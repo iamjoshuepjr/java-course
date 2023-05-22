10cb3ef905b8b653530d25d1d09c0d40dbd6b135
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-22 14:44:41 -0500

    OOP - Inheritace Exercises - (Vehicle)

--- src/javaObjectOrientedProgramming/exercises/inheritance/classes/vehicle/Vehicle.java
+++ src/javaObjectOrientedProgramming/exercises/inheritance/classes/vehicle/Vehicle.java
@@ -1,4 +1,23 @@
 package javaObjectOrientedProgramming.exercises.inheritance.classes.vehicle;
 
+// Object Class
 public class Vehicle {
+    // Attributes
+    private String registrationNumber;
+    private String make;
+    private String model;
+    private short year;
+
+    // Constructor
+    public Vehicle(String registrationNumber, String make, String model, short year){
+        this.registrationNumber = registrationNumber;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Methods
+    public void calculateRentalCost(){
+
+    }
 }
