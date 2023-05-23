2c35038a5544b4214bda8638c1ff38d17d5cd551
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-23 06:23:14 -0500

    OOP - Inheritace Exercises - (Vehicle)

--- src/javaObjectOrientedProgramming/exercises/inheritance/classes/vehicle/Motorcycle.java
+++ src/javaObjectOrientedProgramming/exercises/inheritance/classes/vehicle/Motorcycle.java
@@ -1,2 +1,19 @@
-package javaObjectOrientedProgramming.exercises.inheritance.classes.vehicle;public class Motorcycle {
+package javaObjectOrientedProgramming.exercises.inheritance.classes.vehicle;
+
+// Subclass
+public class Motorcycle extends Vehicle{
+    private float engineCapacity;
+
+    public Motorcycle(float rentalRatePrice, String registrationNumber, String make, String model, short year, float engineCapacity){
+        super(rentalRatePrice, registrationNumber, make, model, year);
+        this.engineCapacity = engineCapacity;
+    }
+
+    public float getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(float engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
 }
--- src/javaObjectOrientedProgramming/exercises/inheritance/classes/vehicle/Truck.java
+++ src/javaObjectOrientedProgramming/exercises/inheritance/classes/vehicle/Truck.java
@@ -1,2 +1,28 @@
-package javaObjectOrientedProgramming.exercises.inheritance.classes.vehicle;public class Truck {
+package javaObjectOrientedProgramming.exercises.inheritance.classes.vehicle;
+
+public class Truck extends Vehicle{
+    private float cargoCapacity;
+
+
+    // Constructor
+    public Truck(float rentalRatePrice, String registrationNumber, String make, String model, short year, float cargoCapacity){
+        super(rentalRatePrice, registrationNumber, make, model, year);
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    // Getter and Setters
+    public float getCargoCapacity(){
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(float cargoCapacity){
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    // Override calculateRentalCost()
+
+    @Override
+    public void calculateRentalCost(float time) {
+        super.calculateRentalCost(time);
+    }
 }
