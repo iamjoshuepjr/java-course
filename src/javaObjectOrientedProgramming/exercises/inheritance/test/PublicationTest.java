05985b0030e3104337c34871debccaa393acbc53
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-23 23:49:28 -0500

    OOP - Inheritace Exercises - (Publication)

--- src/javaObjectOrientedProgramming/exercises/inheritance/test/PublicationTest.java
+++ src/javaObjectOrientedProgramming/exercises/inheritance/test/PublicationTest.java
@@ -1,2 +1,15 @@
-package javaObjectOrientedProgramming.exercises.inheritance.test;public class PublicationTest {
+package javaObjectOrientedProgramming.exercises.inheritance.test;
+
+import javaObjectOrientedProgramming.exercises.inheritance.classes.publication.Book;
+import javaObjectOrientedProgramming.exercises.inheritance.classes.publication.CD;
+
+// Controller Class
+public class PublicationTest {
+    public static void main(String[] args) {
+        Book book = new Book("Think and grow rich", 23.5f, 299);
+        book.information();
+
+        CD cd = new CD("Rich Dad, poor dad", 34.2f, 299);
+        cd.play();
+    }
 }
