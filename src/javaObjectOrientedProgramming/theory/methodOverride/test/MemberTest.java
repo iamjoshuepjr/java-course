49f3da83ccd4ad143d7e7ceced951fee825b2878
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-24 12:43:48 -0500

    OOP Methods Override

--- src/javaObjectOrientedProgramming/theory/methodOverride/test/MemberTest.java
+++ src/javaObjectOrientedProgramming/theory/methodOverride/test/MemberTest.java
@@ -1,2 +1,16 @@
-package javaObjectOrientedProgramming.theory.methodOverride.test;public class MemberTest {
+package javaObjectOrientedProgramming.theory.methodOverride.test;
+
+import javaObjectOrientedProgramming.theory.methodOverride.classes.member.RegularMember;
+import javaObjectOrientedProgramming.theory.methodOverride.classes.member.VolunteerMember;
+
+// Controller Class
+public class MemberTest {
+    public static void main(String[] args) {
+        RegularMember regularMember = new RegularMember("John Smith", (byte) 30, "123 Main St", "555-1234", "2022-01-01");
+        regularMember.information();
+
+        String[] areasOfService = {"Children's Ministry", "Hospital Visitation"};
+        VolunteerMember volunteerMember = new VolunteerMember("Jane Doe", (byte) 25, "456 Elm St", "555-5678", areasOfService);
+        volunteerMember.information();
+    }
 }
