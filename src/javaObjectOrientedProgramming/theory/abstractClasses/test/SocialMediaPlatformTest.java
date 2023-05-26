e2fafbce0f25a4b4d690aa04286d2916cb8da021
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-26 12:16:29 -0500

    OOP Abstract Classes (Meta Abstract Class)

--- src/javaObjectOrientedProgramming/theory/abstractClasses/test/SocialMediaPlatformTest.java
+++ src/javaObjectOrientedProgramming/theory/abstractClasses/test/SocialMediaPlatformTest.java
@@ -3,14 +3,14 @@ package javaObjectOrientedProgramming.theory.abstractClasses.test;
 import javaObjectOrientedProgramming.theory.abstractClasses.classes.meta.Facebook;
 import javaObjectOrientedProgramming.theory.abstractClasses.classes.meta.Instagram;
 import javaObjectOrientedProgramming.theory.abstractClasses.classes.meta.WhatsApp;
-import javaObjectOrientedProgramming.theory.abstractClasses.classes.meta.abstracts.SocialMediaPlatform;
+import javaObjectOrientedProgramming.theory.abstractClasses.classes.meta.abstracts.Meta;
 
 import javax.swing.JOptionPane;
 
 // Controller Class
 public class SocialMediaPlatformTest {
 
-    public static void display(SocialMediaPlatform platform){
+    public static void display(Meta platform){
         JOptionPane.showMessageDialog(null, "SOCIAL MEDIA INFORMATION" +
           "\nPlatform Name: " + platform.getPlatformName() +
           "\nTotal Users: " + platform.getTotalUsers()
@@ -18,9 +18,9 @@ public class SocialMediaPlatformTest {
     }
     public static void main(String[] args) {
         // Polymorphism between classes
-        SocialMediaPlatform facebook = new Facebook("Facebook", 2500000000L);
-        SocialMediaPlatform twitter = new WhatsApp("Twitter", 330000000L);
-        SocialMediaPlatform instagram = new Instagram("Instagram", 1000000000L);
+        Meta facebook = new Facebook("Facebook", 2500000000L);
+        Meta twitter = new WhatsApp("Twitter", 330000000L);
+        Meta instagram = new Instagram("Instagram", 1000000000L);
 
         display(facebook);
         display(twitter);
