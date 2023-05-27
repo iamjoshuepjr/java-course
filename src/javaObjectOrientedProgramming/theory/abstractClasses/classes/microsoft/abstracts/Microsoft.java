e844e1c93d78ca1821a80a9411f94ff24892db99
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-27 08:30:04 -0500

    OOP - Abstract Classes (Microsoft Abstract Class)

--- src/javaObjectOrientedProgramming/theory/abstractClasses/classes/microsoft/abstracts/Microsoft.java
+++ src/javaObjectOrientedProgramming/theory/abstractClasses/classes/microsoft/abstracts/Microsoft.java
@@ -1,2 +1,37 @@
-package javaObjectOrientedProgramming.theory.abstractClasses.classes.microsoft.abstracts;public class Microsoft {
+package javaObjectOrientedProgramming.theory.abstractClasses.classes.microsoft.abstracts;
+
+// Superclass (Abstract Class)
+// Object Class
+
+public abstract class Microsoft {
+    // Attributes
+    private String name;
+    private short foundingYear;
+
+    // Constructor
+    public Microsoft(String name, short foundingYear){
+        this.name = name;
+        this.foundingYear = foundingYear;
+    }
+
+    // Concrete Methods
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public short getFoundingYear() {
+        return foundingYear;
+    }
+
+    public void setFoundingYear(short foundingYear) {
+        this.foundingYear = foundingYear;
+    }
+
+    // Abstract Methods
+    public abstract void information();
 }
