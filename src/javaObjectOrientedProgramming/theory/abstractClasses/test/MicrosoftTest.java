e3a295138ed7ff1812fa42bae63e4e35aece59f6
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-27 08:31:02 -0500

    OOP - Abstract Classes (MicrosoftTest)

--- src/javaObjectOrientedProgramming/theory/abstractClasses/test/MicrosoftTest.java
+++ src/javaObjectOrientedProgramming/theory/abstractClasses/test/MicrosoftTest.java
@@ -1,2 +1,28 @@
-package javaObjectOrientedProgramming.theory.abstractClasses.test;public class MicrosoftTest {
+package javaObjectOrientedProgramming.theory.abstractClasses.test;
+
+import javaObjectOrientedProgramming.theory.abstractClasses.classes.microsoft.*;
+import javaObjectOrientedProgramming.theory.abstractClasses.classes.microsoft.abstracts.Microsoft;
+
+// Controller Class
+public class MicrosoftTest {
+    public static void main(String[] args) {
+        Microsoft windows = new Windows("Windows", (short) 1975, "Windows 11");
+
+        String[][] services = {
+                {"Compute", "Virtual Machines", "Azure Kubernetes Service (AKS)"},
+                {"Storage", "Azure Files", "Azure Disk Storage"},
+                {"Databases", "AzureSQL Database", "Azure Database for MySQL"}
+        };
+
+        Microsoft azure = new Azure("Azure", (short) 2010, services);
+        Microsoft linkedIn = new LinkedIn("LinkedIn", (short) 2002, 740000000);
+        Microsoft gitHub = new GitHub("GitHub", (short) 2008, "100 million repositories");
+        Microsoft skype = new Skype("Skype", (short) 2003, true);
+
+        Microsoft[] divisions = {azure, linkedIn, gitHub, skype};
+
+        for (Microsoft division : divisions ) {
+            division.information();
+        }
+    }
 }
