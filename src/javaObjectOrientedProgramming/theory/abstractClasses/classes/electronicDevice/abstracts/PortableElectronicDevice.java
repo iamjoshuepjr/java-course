69a9902e56fd98fc94bb248d76d204e1cfe93ad6
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-27 14:09:12 -0500

    OOP - Abstract Classes (PortableElectronicDevice Abstract Class)

--- src/javaObjectOrientedProgramming/theory/abstractClasses/classes/electronicDevice/abstracts/PortableElectronicDevice.java
+++ src/javaObjectOrientedProgramming/theory/abstractClasses/classes/electronicDevice/abstracts/PortableElectronicDevice.java
@@ -4,4 +4,6 @@ public abstract class PortableElectronicDevice extends ElectronicDevice{
     public PortableElectronicDevice(String brand, String model, short year) {
         super(brand, model, year);
     }
+
+    public abstract void charge();
 }
