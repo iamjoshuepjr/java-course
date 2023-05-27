3e576e60950387ffa6c7d149b1ada8ece0876d71
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-27 14:34:48 -0500

    OOP - Abstract Classes (PortableElectronicDevice Child Class)

--- src/javaObjectOrientedProgramming/theory/abstractClasses/classes/electronicDevice/Smartphone.java
+++ src/javaObjectOrientedProgramming/theory/abstractClasses/classes/electronicDevice/Smartphone.java
@@ -1,2 +1,45 @@
-package javaObjectOrientedProgramming.theory.abstractClasses.classes.electronicDevice;public class Smartphone {
+package javaObjectOrientedProgramming.theory.abstractClasses.classes.electronicDevice;
+
+import javaObjectOrientedProgramming.theory.abstractClasses.classes.electronicDevice.abstracts.PortableElectronicDevice;
+import javax.swing.JOptionPane;
+
+public class Smartphone extends PortableElectronicDevice {
+
+    // Constructor
+    public Smartphone(String brand, String model, short year) {
+        super(brand, model, year);
+    }
+
+    @Override
+    public void turnOn() {
+        JOptionPane.showMessageDialog(null, "TURNING ON SMARTPHONE " +
+                "\nBrand: " + getBrand() +
+                "\nModel: " + getModel()
+        );
+    }
+
+    @Override
+    public void turnOff() {
+        JOptionPane.showMessageDialog(null, "TURNING OFF SMARTPHONE " +
+                "\nBrand: " + getBrand() +
+                "\nModel: " + getModel()
+        );
+    }
+
+    @Override
+    public void charge() {
+        JOptionPane.showMessageDialog(null, "CHARGING SMARTPHONE " +
+                "\nBrand: " + getBrand() +
+                "\nModel: " + getModel()
+        );
+    }
+
+    @Override
+    public void information() {
+        JOptionPane.showMessageDialog(null, "SMARTPHONE INFORMATION " +
+                "\nBrand: " + getBrand() +
+                "\nModel: " + getModel() +
+                "\nYear: " + getYear()
+        );
+    }
 }
--- src/javaObjectOrientedProgramming/theory/abstractClasses/classes/electronicDevice/Tablet.java
+++ src/javaObjectOrientedProgramming/theory/abstractClasses/classes/electronicDevice/Tablet.java
@@ -1,2 +1,45 @@
-package javaObjectOrientedProgramming.theory.abstractClasses.classes.electronicDevice;public class Tablet {
+package javaObjectOrientedProgramming.theory.abstractClasses.classes.electronicDevice;
+
+import javaObjectOrientedProgramming.theory.abstractClasses.classes.electronicDevice.abstracts.PortableElectronicDevice;
+import javax.swing.JOptionPane;
+
+public class Tablet extends PortableElectronicDevice {
+
+    // Constructor
+    public Tablet(String brand, String model, short year) {
+        super(brand, model, year);
+    }
+
+    @Override
+    public void turnOn() {
+        JOptionPane.showMessageDialog(null, "TURNING ON TABLET " +
+                "\nBrand: " + getBrand() +
+                "\nModel: " + getModel()
+        );
+    }
+
+    @Override
+    public void turnOff() {
+        JOptionPane.showMessageDialog(null, "TURNING ON TABLET " +
+                "\nBrand: " + getBrand() +
+                "\nModel: " + getModel()
+        );
+    }
+
+    @Override
+    public void charge() {
+        JOptionPane.showMessageDialog(null, "CHARGING TABLET " +
+                "\nBrand: " + getBrand() +
+                "\nModel: " + getModel()
+        );
+    }
+
+    @Override
+    public void information() {
+        JOptionPane.showMessageDialog(null, "TABLET INFORMATION " +
+                "\nBrand: " + getBrand() +
+                "\nModel: " + getModel() +
+                "\nYear: " + getYear()
+        );
+    }
 }
