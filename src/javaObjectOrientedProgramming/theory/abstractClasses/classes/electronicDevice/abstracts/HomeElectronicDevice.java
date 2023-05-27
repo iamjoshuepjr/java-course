484d88e30732ee02bc91a5fda3d0efe940ad5f1c
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-27 14:18:53 -0500

    OOP - Abstract Classes (HomeElectronicDevice Abstract Class)

--- src/javaObjectOrientedProgramming/theory/abstractClasses/classes/electronicDevice/abstracts/HomeElectronicDevice.java
+++ src/javaObjectOrientedProgramming/theory/abstractClasses/classes/electronicDevice/abstracts/HomeElectronicDevice.java
@@ -1,7 +1,7 @@
 package javaObjectOrientedProgramming.theory.abstractClasses.classes.electronicDevice.abstracts;
 
 public abstract class HomeElectronicDevice extends ElectronicDevice {
-    
+
     // Constructor
     public HomeElectronicDevice(String brand, String model, short year) {
         super(brand, model, year);
