2e7d5ed9f4834b367f0a967ac6016d42a1d67d71
joshuepjr <mycoursesonline2021@gmail.com>
2023-05-30 13:26:28 -0500

    OOP - Abstract Classes (EmployeeTest Class)

--- src/javaObjectOrientedProgramming/exercises/abstractClasses/test/EmployeeTest.java
+++ src/javaObjectOrientedProgramming/exercises/abstractClasses/test/EmployeeTest.java
@@ -4,23 +4,27 @@ import javaObjectOrientedProgramming.exercises.abstractClasses.classes.*;
 import javaObjectOrientedProgramming.exercises.abstractClasses.classes.abstracts.Employee;
 
 import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
 
 // Controller Class
 public class EmployeeTest {
     public static void main(String[] args) {
         Employee salaried = new SalariedEmployee(123, "Karen", 5000);
-        Employee hourly = new HourlyEmployee(124, "Dayanna", 30.8f, (byte) 40);
-        Employee commission = new CommissionEmployee(125, "Lucia", 400000, 450);
-        Employee hourCommission = new SalaryPlusCommissionEmployee(126, "Luisa", 4890, 500000, 500);
+        Employee hourly = new HourlyEmployee(124, "Dayanna", 30.8f, (byte) 40f);
+        Employee commission = new CommissionEmployee(125, "Lucia", 400000f, 450f);
+        Employee hourCommission = new SalaryPlusCommissionEmployee(126, "Luisa", 4890f, 500000f, 500f);
 
         Employee[] employees = {salaried, hourly, commission, hourCommission};
 
-        int totalPayRoll = 0;
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        float totalPayRoll = 0.0f;
+
         for (Employee e: employees) {
             e.payRollDetail();
             totalPayRoll += e.calculatePay();
         }
+        String formattedTotalPayRoll = decimalFormat.format(totalPayRoll);
         JOptionPane.showMessageDialog(null, "TOTAL PAYROLL " +
-                "\n$ " + totalPayRoll);
+                "\n$ " + formattedTotalPayRoll);
     }
 }
