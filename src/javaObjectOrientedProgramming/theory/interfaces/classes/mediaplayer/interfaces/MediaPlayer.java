ec6bbf9ccfa4431e0608fe1ae94f425bbb7d9572
joshuepjr <mycoursesonline2021@gmail.com>
2023-06-02 22:54:07 -0500

    OOP - Interfaces (MediaPlayer Interface)

---
+++ src/javaObjectOrientedProgramming/theory/interfaces/classes/mediaplayer/interfaces/MediaPlayer.java
@@ -0,0 +1,11 @@
+package javaObjectOrientedProgramming.theory.interfaces.classes.mediaplayer.interfaces;
+
+// Declaring an interface
+public interface MediaPlayer {
+    // Abstract methods declaration
+    void play();
+    void pause();
+    void stop();
+    void next();
+    void previous();
+}
