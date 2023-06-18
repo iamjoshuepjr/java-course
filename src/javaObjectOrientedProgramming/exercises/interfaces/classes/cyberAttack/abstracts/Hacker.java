0910d97659b5279d88a67b8c6786c37933c8e08f
joshuepjr <mycoursesonline2021@gmail.com>
2023-06-17 19:06:21 -0500

    OOP - Interfaces (Hacker Abstract Class)

--- src/javaObjectOrientedProgramming/exercises/interfaces/classes/cyberAttack/abstracts/Hacker.java
+++ src/javaObjectOrientedProgramming/exercises/interfaces/classes/cyberAttack/abstracts/Hacker.java
@@ -2,9 +2,12 @@ package javaObjectOrientedProgramming.exercises.interfaces.classes.cyberAttack.a
 
 import javaObjectOrientedProgramming.exercises.interfaces.classes.cyberAttack.enums.*;
 import javaObjectOrientedProgramming.exercises.interfaces.classes.cyberAttack.interfaces.ICyberAttack;
+import javax.swing.JOptionPane;
 
 public abstract class Hacker implements ICyberAttack {
     // Attributes
+    private static int lastAssignedId = 0;
+    private int id;
     private String alias;
     private Motivation motivation;
     private Techniques techniques;
@@ -12,9 +15,25 @@ public abstract class Hacker implements ICyberAttack {
 
     // Constructor
     public Hacker(String alias, Motivation motivation, Techniques techniques, Experience levelExperience) {
+        this.id = ++lastAssignedId;
         this.alias = alias;
         this.motivation = motivation;
         this.techniques = techniques;
         this.levelExperience = levelExperience;
     }
+
+    // private methods
+    private void showMessage(String message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
+
+    public void information(){
+        showMessage("HACKER INFORMATION\n" +
+                "Id: " + id +
+                "\nAlias: " + alias +
+                "\nMotivation: " + motivation +
+                "\nTechniques: " + techniques +
+                "\nLevel Experience: " + levelExperience);
+    }
+
 }
