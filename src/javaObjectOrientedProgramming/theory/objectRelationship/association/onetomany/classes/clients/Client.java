207b8fa5f5f3478695aa5bbc840bc5684a375f63
joshuepjr <mycoursesonline2021@gmail.com>
2023-06-30 21:07:34 -0500

    OOP - Object Relationship (Association) [one-to-many-cardinality]

---
+++ src/javaObjectOrientedProgramming/theory/objectRelationship/association/onetomany/classes/clients/Client.java
@@ -0,0 +1,44 @@
+package javaObjectOrientedProgramming.theory.objectRelationship.association.onetomany.classes.clients;
+
+import java.util.List;
+
+// Object Class
+public class Client {
+    // Attributes
+    private String id;
+    private String name;
+
+    private List<Product> products;
+
+    // Empty Constructor
+    public Client(){}
+
+    // Overloaded Constructor
+    public Client(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return  "\nId: " +id+ "\n" +
+                "Name: " +name+ "\n" +
+                "Products: " +products + "\n";
+    }
+}
