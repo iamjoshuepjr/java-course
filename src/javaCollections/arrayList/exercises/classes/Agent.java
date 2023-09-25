1d8a00dafdce6093587dee9f5ee6003927c1a36b
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-09-24 20:57:00 -0500

    Java Collections[ArrayList]

---
+++ src/javaCollections/arrayList/exercises/classes/Agent.java
@@ -0,0 +1,54 @@
+package javaCollections.arrayList.exercises.classes;
+
+// Object Class
+public class Agent {
+    // Attributes
+    private Integer id;
+    private String name;
+    private String login;
+    private String extension;
+    private String skill;
+
+    // Empty Constructor
+    public Agent(){}
+
+    // Overloaded Constructor
+    public Agent(Integer id, String name, String login, String extension){
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.extension = extension;
+    }
+
+    // Getters and Setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    @Override
+    public String toString() {
+        return  "\nId: " +id+ "\n" +
+                "Name: " +name+ "\n" +
+                "Login: " +login+ "\n" +
+                "Extension: " +extension+ "\n" +
+                "Skill: " +skill+ "\n";
+    }
+}
+
+
