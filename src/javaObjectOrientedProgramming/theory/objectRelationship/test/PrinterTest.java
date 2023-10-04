5ad93f71af8e9f8f0643223558322d57070cc730
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-03 23:09:03 -0500

    OOP - Object Relationship[Use-Dependency(PrinterTest)]

---
+++ src/javaObjectOrientedProgramming/theory/objectRelationship/test/PrinterTest.java
@@ -0,0 +1,13 @@
+package javaObjectOrientedProgramming.theory.objectRelationship.test;
+
+import javaObjectOrientedProgramming.theory.objectRelationship.useDependency.Document;
+import javaObjectOrientedProgramming.theory.objectRelationship.useDependency.Printer;
+
+public class PrinterTest {
+    public static void main(String[] args) {
+        Document document = new Document("The Game", "This book has changed a great amount of lives!");
+        Printer printer = new Printer();
+
+        printer.toPrint(document);
+    }
+}
