2516108ccc05584b705ac2cb412b7120e591cc39
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-03 23:08:09 -0500

    OOP - Object Relationship[Use-Dependency(Printer)]

---
+++ src/javaObjectOrientedProgramming/theory/objectRelationship/useDependency/Printer.java
@@ -0,0 +1,21 @@
+package javaObjectOrientedProgramming.theory.objectRelationship.useDependency;
+
+import javax.swing.*;
+
+public class Printer {
+    private boolean isOn = false;
+
+    public void turnOn(){
+        isOn = true;
+    }
+
+    public void toPrint(Document document){
+        if(isOn){
+            JOptionPane.showMessageDialog(null, "Printing Document..." +
+                    "\t"+ document.getTitle() + "\n\t" + document.getBody());
+        } else {
+            JOptionPane.showMessageDialog(null, "The printer is turned off!\nDo you want to turn it on?");
+        }
+    }
+
+}
