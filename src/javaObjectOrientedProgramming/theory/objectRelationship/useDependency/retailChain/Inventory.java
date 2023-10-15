0db02fd2ddffa96af524c99343d6f09c8152cff0
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-15 16:22:01 -0500

    OOP - Object Relationship[Dependency(Inventory)]

--- src/javaObjectOrientedProgramming/theory/objectRelationship/useDependency/retailChain/Inventory.java
+++ src/javaObjectOrientedProgramming/theory/objectRelationship/useDependency/retailChain/Inventory.java
@@ -1,6 +1,6 @@
 package javaObjectOrientedProgramming.theory.objectRelationship.useDependency.retailChain;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class Inventory {
     private Product product;
