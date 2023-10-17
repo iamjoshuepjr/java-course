8cbb1eb9155c6824725d93a29e9e23cc6d60076a
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-17 07:38:28 -0500

    Java GUI -Fundamentals

---
+++ src/javaGUI/fundamentals/Window.java
@@ -0,0 +1,13 @@
+package javaGUI.fundamentals;
+
+import javax.swing.*;
+public class Window extends JFrame {
+    public Window() {
+        setSize(500, 300); // Allows us to set the size of the frame; width and height in pixels.
+        setLocation(500, 400);
+        setVisible(true); // Make the frame visible.
+        setResizable(false); // Allows us to enable or disable the frame's ability to be resized by the user.
+        setDefaultCloseOperation(3); // Set the default close operation to exit the application
+        setTitle("Orange App");
+    }
+}
