377a0e5e533f22ac35f6c3d3839c6f1c4939c8d1
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-17 07:38:38 -0500

    Java GUI -Fundamentals

---
+++ src/javaGUI/fundamentals/WindowTest.java
@@ -0,0 +1,9 @@
+package javaGUI.fundamentals;
+
+import javaGUI.fundamentals.Window;
+
+public class WindowTest {
+    public static void main(String[] args) {
+        Window window = new Window();
+    }
+}
