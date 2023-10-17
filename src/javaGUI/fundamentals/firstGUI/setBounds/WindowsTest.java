56375be6a6fa55318d4fbba9618e4d9724749f5a
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-17 08:17:23 -0500

    Java GUI - Fundamentals(SetBounds)

---
+++ src/javaGUI/fundamentals/firstGUI/setBounds/WindowsTest.java
@@ -0,0 +1,9 @@
+package javaGUI.fundamentals.firstGUI.setBounds;
+
+import javaGUI.fundamentals.firstGUI.setBounds.Window;
+
+public class WindowsTest {
+    public static void main(String[] args) {
+        Window window = new Window();
+    }
+}
