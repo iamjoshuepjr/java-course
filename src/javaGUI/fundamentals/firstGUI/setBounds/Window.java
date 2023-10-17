17bc0dedd04556526aaab75a227c415f748a2229
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-17 08:22:12 -0500

    Java GUI - Fundamentals(SetBounds)

--- src/javaGUI/fundamentals/firstGUI/setBounds/Window.java
+++ src/javaGUI/fundamentals/firstGUI/setBounds/Window.java
@@ -4,9 +4,9 @@ import javax.swing.*;
 
 public class Window extends JFrame {
     public Window(){
-        setBounds(500, 400, 500, 400);
+        setBounds(500, 400, 500, 400); // Allows us set both the position and size of the frame
         setVisible(true);
         setDefaultCloseOperation(3);
-        setTitle("Orange Bank");
+        setTitle("Orange App");
     }
 }
