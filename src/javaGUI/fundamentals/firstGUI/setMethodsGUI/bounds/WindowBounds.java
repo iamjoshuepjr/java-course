cb2abc714b7e05f4daa1e7bac8bdea5f651802eb
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-18 10:43:09 -0500

    Java GUI - Fundamentals(SetBounds)

--- src/javaGUI/fundamentals/firstGUI/setMethodsGUI/bounds/WindowBounds.java
+++ src/javaGUI/fundamentals/firstGUI/setMethodsGUI/bounds/WindowBounds.java
@@ -1,12 +1,14 @@
 package javaGUI.fundamentals.firstGUI.setMethodsGUI.bounds;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WindowBounds extends JFrame {
     public WindowBounds(){
-        setBounds(500, 400, 500, 400); // Allows us set both the position and size of the frame
+        setBounds(200, 40, 500, 400); // Allows us set both the position and size of the frame
         setVisible(true);
         setDefaultCloseOperation(3);
+        setExtendedState(Frame.MAXIMIZED_BOTH); // Maximized the frame
         setTitle("Orange App");
     }
 }
