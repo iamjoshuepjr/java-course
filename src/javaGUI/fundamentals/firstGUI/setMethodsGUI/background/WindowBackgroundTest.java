e06996afee27d28c3559d7e8f363debe894e60cb
iamjoshuepjr <mycoursesonline2021@gmail.com>
2023-10-23 22:48:24 -0500

    Java GUI - Fundamentals(SetBackgraound)

---
+++ src/javaGUI/fundamentals/firstGUI/setMethodsGUI/background/WindowBackgroundTest.java
@@ -0,0 +1,7 @@
+package javaGUI.fundamentals.firstGUI.setMethodsGUI.background;
+
+public class WindowBackgroundTest {
+    public static void main(String[] args) {
+        WindowBackground windowBackground = new WindowBackground();
+    }
+}
